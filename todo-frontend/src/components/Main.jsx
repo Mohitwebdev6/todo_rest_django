38e143c0a0d6041461b7ef130381e74c687f2b5f
Mohitwebdev6 <mohit.webdev6@gmail.com>
2024-05-16 15:08:01 +0530

    some features added to our frontend

--- todo-frontend/src/components/Main.jsx
+++ todo-frontend/src/components/Main.jsx
@@ -1,49 +1,53 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-export default function Main() {
-  const [todos, setTodos] = useState([]);
-  const [error, setError] = useState([]);
+export default function Main({ item, formattedDate }) {
+  
+  const [disable, setDisable] = useState(true)
+  const [data, setData] = useState({ "title": item.title, "description": item.description, "is_completed": item.is_completed })
+
+  const toggleComplete = () => {
+    setData(prev => ({ ...prev, "is_completed": !prev.is_completed }))
+  }
 
   useEffect(() => {
-    const fetch_data = async () => {
-      try {
-        const response = await fetch("http://127.0.0.1:8000/api/todo_list/");
-        if (!response.ok) {
-          throw new Error(response.error);
-        }
-        const data = await response.json();
-        setTodos(data);
-      } catch (error) {
-        setError(error);
-      }
-    };
-    fetch_data();
-  }, []);
+    fetch(`http://127.0.0.1:8000/api/v1/todo/${item.id}/`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }, [data])
+
+  const toggleEdit = (e) => {
+    setDisable((val) => !val)
+    if (!disable) {
+      e.target.innerText = "edit"
+    }
+    else {
+      e.target.innerText = "update"
+    }
+  }
+
   return (
-    <main>
-      <ul className="w-[50%] mx-auto flex flex-col gap-3 my-4">
-        {todos.map((item) => {
-          const date = new Date(item.created_at);
-          const formattedDate = date.toLocaleString();
-          return (
-            <li key={item.id} className="bg-slate-200 rounded-2xl p-3">
-              <h1 className="text-3xl">{item.title}</h1>
-              <h3>{item.description}</h3>
-              {item.is_completed ? (
-                <li className="bg-green-500 p-3 text-white w-[20%] text-center rounded-2xl">
-                  Completed
-                </li>
-              ) : (
-                <li className="bg-red-500 p-3 text-white w-[20%] text-center rounded-2xl">
-                  Not Completed
-                </li>
-              )}
-              <h3>{formattedDate}</h3>
-            </li>
-          );
-        })}
-      </ul>
-    </main>
+    <li key={item.id} className="bg-slate-200 rounded-2xl p-3 list-none">
+      <input type="text" name="title" value={data.title} onChange={(e)=>{setData(prev=>({...prev,"title":e.target.value}))}} disabled={disable} /><br />
+      <textarea name="description" value={data.description} onChange={(e) => { setData((prev) => ({ ...prev, "description": e.target.value })) }} disabled={disable} ></textarea><br />
+      {data.is_completed ? (
+        <button className="bg-green-500 p-3 text-white w-[20%] text-center rounded-2xl" onClick={toggleComplete} >
+          Completed
+        </button>
+      ) : (
+        <button className="bg-red-500 p-3 text-white w-[20%] text-center rounded-2xl" onClick={toggleComplete} >
+          Not Completed
+        </button>
+      )}
+      <h3>{formattedDate}</h3>
+
+      <button className=" rounded-lg p-2 bg-blue-400 px-5 text-white" onClick={(e) => { toggleEdit(e) }}>Edit</button>
+    </li>
   );
 }
--- todo-frontend/src/pages/Home.jsx
+++ todo-frontend/src/pages/Home.jsx
@@ -2,12 +2,35 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Main from '../components/Main'
+import { useState,useEffect } from 'react'
 
 export default function Home() {
+    const [todos, setTodos] = useState([]);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+       fetch("http://127.0.0.1:8000/api/v1/todo/")
+       .then((res)=>{
+        return res.json()
+       })
+       .then((res)=>{
+        setTodos(res)
+       })
+       .catch(err=>{
+        setError(err.message)
+        
+       })
+    }, []);
     return (
         <div>
             <Navbar></Navbar>
-            <Main></Main>
+            <ul className="w-[50%] mx-auto flex flex-col gap-3 my-4 list-none min-h-lvh justify-center">
+                {error?<div className=' bg-red-300 text-center p-4 rounded-lg'>{error}</div>:(todos.map((item) => {
+                    const date = new Date(item.created_at);
+                    const formattedDate = date.toLocaleString();
+                    return (<Main formattedDate={formattedDate} key={item.id} item={item}></Main>)
+                }))}
+                
+            </ul>
             <Footer></Footer>
         </div>
     )
