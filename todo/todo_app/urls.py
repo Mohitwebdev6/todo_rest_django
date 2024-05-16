c7357015d12cabeffe57338a2930c71f5fea5857
Mohitwebdev6 <mohit.webdev6@gmail.com>
2024-05-16 13:05:55 +0530

    optimazations done

--- todo/todo_app/urls.py
+++ todo/todo_app/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("todo_list/",views.todo_list,name="todo_list"),
-    path("add_todo/",views.add_todo,name="add_todo"),
-    path("delete_todo/<id>/",views.delete_todo,name="delete_todo"),
-    path("update_todo/<id>/",views.update_todo,name="update_todo"),
-
+    path("todo/",views.todo,name="todo"),
+    path("todo/<id>/",views.todo_methods,name="todo_methods"),
 ]
